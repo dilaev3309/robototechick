a5ca86d24de77f1e673c25b3c36c809e06235bca
dilaev3309 <124659508+dilaev3309@users.noreply.github.com>
2023-11-24 15:19:57 +0700

    Add files via upload

---
+++ ex05/task4/ex5/launch/sqare_movement.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    robot_nodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('robot_bringup'), 'launch'),
+                                       '/diff_drive.launch.py']),
+    )
+
+
+    return LaunchDescription([
+        robot_nodes,
+        Node(
+            package='ex5',
+            executable='sqare_movement',
+            name='mvm',
+        ),
+    ])
---
+++ ex05/task4/ex5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'ex5'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Denis',
+    maintainer_email='d.milaev@g.nsu.ru',
+    description='ex4',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ex5 = ex5.ex5:main'
+        ],
+    },
+)
